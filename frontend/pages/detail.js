af3b5387395b912074fd5731328ab6c450d6bb84
José Antonio Salinas Ochoa <antonio.salinas@ucamp.utel.edu.mx>
2021-12-03 00:01:38 -0600

    Error en detail.js

--- frontend/pages/detail.js
+++ frontend/pages/detail.js
@@ -12,7 +12,7 @@ import styles from './../components/Principal/Principal.module.scss'
 
 export default function Detail() {
     let search = null;
-    if ( window != 'undefined' ) {
+    if ( window != null ) {
          search = window.location.search;
     }
     //console.log('Error',window.location.search)
